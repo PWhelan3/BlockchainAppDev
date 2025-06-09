bc2c8f93c9cafb5fa0c8432208660567d119f8ce
Peter <pwhelanstudio@gmail.com>
2025-06-09 14:49:01 +0100

    Add Navbar to page.js

--- client/app/page.js
+++ client/app/page.js
@@ -3,10 +3,14 @@ import Navbar from "./components/Navbar";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-900 text-white">
-      <h1 className="text-4xl font-bold mb-4">🎟️ Welcome to Current</h1>
-      <p className="text-lg text-gray-300">Your Web3-powered community event platform.</p>
+    <div>
+      <Navbar></Navbar>
+        <div className="flex flex-col items-center justify-center min-h-screen bg-gray-900 text-white">
+        <h1 className="text-4xl font-bold mb-4">🎟️ Welcome to Current</h1>
+        <p className="text-lg text-gray-300">Your Web3-powered community event platform.</p>
+      </div>
     </div>
+
   );
 }
 
